bd78154fde88f2d7020efcab568a469197146775
harini-praba-haprix <harini.haprixai@gmail.com>
2026-03-30 15:47:25 +0530

    Update Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
